e955c0fe7a1e43ef4bea2b2ee95fff06f3b09ae9
athena <athena@themainframe.com>
2020-09-26 16:19:40 +0000

    Finished scatter, line and bar charts

--- 1-02E-ScatterplotExercises/Ex1-Scatterplot.py
+++ 1-02E-ScatterplotExercises/Ex1-Scatterplot.py
@@ -7,21 +7,20 @@
 ######
 
 # Perform imports here:
-
-
-
+import plotly.offline as pyo
+import plotly .graph_objs as go
+import numpy as np
 
 # Define a data variable
+x = np.random.randn(1000)
+y = np.random.rand(1000)
 
-
-
-
-
+data = [go.Scatter(x=x, y=y, mode='markers')]
 
 # Define the layout
-
-
-
+layout = go.Layout(title='Scatterplot', xaxis=dict(title='x'), yaxis=dict(title='y'), hovermode='closest')
 
 
 # Create a fig from data and layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='results/1-02E_scatterplot.html')
--- 1-03E-LineChartExercises/Ex2-Linechart.py
+++ 1-03E-LineChartExercises/Ex2-Linechart.py
@@ -5,27 +5,31 @@
 ######
 
 # Perform imports here:
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # Create a pandas DataFrame from 2010YumaAZ.csv
-df = pd.read_csv('../data/2010YumaAZ.csv')
+df = pd.read_csv('data/2010YumaAZ.csv')
 days = ['TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','MONDAY']
 
 
 # Use a for loop (or list comprehension to create traces for the data list)
-data = []
+traces = [go.Scatter(x=df[df['DAY']==day]['LST_TIME'],
+                    y=df[df['DAY']==day]['T_HR_AVG'],
+                    mode='lines',
+                    name=day)
+        for day in days]
 
-for day in days:
-    # What should go inside this Scatter call?
-    trace = go.Scatter()
-    data.append(trace)
+# for day in days:
+#     # What should go inside this Scatter call?
+#     trace = go.Scatter()
+#     data.append(trace)
 
 # Define the layout
-
-
-
-
+layout = go.Layout(title='Daily temps')
 
 # Create a fig from data and layout, and plot the fig
+fig = go.Figure(data=traces, layout=layout)
+pyo.plot(fig, filename='results/1-03E_linechart.html')
