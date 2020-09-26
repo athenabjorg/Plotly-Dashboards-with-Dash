252bd51d6a4918ad943bd30538ac60e066101e8c
athena <athena@themainframe.com>
2020-09-26 19:46:17 +0000

    Finished bublechart, boxplot and histogram

--- 1-05E-BubbleChartExercises/Ex4-Bubblechart.py
+++ 1-05E-BubbleChartExercises/Ex4-Bubblechart.py
@@ -5,28 +5,29 @@
 ######
 
 # Perform imports here:
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # create a DataFrame from the .csv file:
+df = pd.read_csv('data/mpg.csv')
 
 
 # create data by choosing fields for x, y and marker size attributes
-
-
-
-
-
-
-
+data = [go.Scatter(x=df['horsepower'],
+                   y=df['acceleration'],
+                   text=df['name'],
+                   mode='markers',
+                   marker=dict(size=df['weight'] / 250)
+                  )
+        ]
 
 # create a layout with a title and axis labels
-
-
-
-
-
+layout = go.Layout(title='Horsepower vs Acceleration (Size by weight)',
+                    hovermode='closest')
 
 
 # create a fig from data & layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='results/1-05_bubblechart.html')
--- 1-06E-BoxPlotExercises/Ex5-Boxplot.py
+++ 1-06E-BoxPlotExercises/Ex5-Boxplot.py
@@ -6,33 +6,25 @@
 ######
 
 # Perform imports here:
-
-
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
 
 # create a DataFrame from the .csv file:
-
+df = pd.read_csv('data/abalone.csv')
 
 # take two random samples of different sizes:
-
-
+sample1 = np.random.choice(df['rings'], 10, replace=False)
+sample2 = np.random.choice(df['rings'], 10, replace=False)
 
 # create a data variable with two Box plots:
-
-
-
-
-
-
-
-
-
-
+data = [go.Box(y=sample1, name='Sample 1'),
+        go.Box(y=sample2, name='Sample 2')]
 
 # add a layout
-
-
-
+layout = go.Layout(title='Rings samples')
 
 # create a fig from data & layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='results/1-06_boxplot.html')
